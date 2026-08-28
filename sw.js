b2c0ecd1ce131ff590f158c9aa44c9c269859803
Beatuy-Rachel <13434391331@163.com>
2026-08-29 00:48:23 +0800

    Polish meal shortcuts and time recording safety

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shiguang-shell-v5';
+const CACHE_NAME = 'shiguang-shell-v6';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
